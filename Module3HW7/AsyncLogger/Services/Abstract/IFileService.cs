aaf2c57069bb4f1aafa64cd5ac78f30cbe3fe57a
Andrew Kara <a.kara701@gmail.com>
2022-01-04 13:27:15 +0200

    Add work with files logic

---
+++ Module3HW7/AsyncLogger/Services/Abstract/IFileService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncLogger.Services.Abstract
+{
+    public interface IFileService
+    {
+        Task CloseFile(IDisposable stream);
+        IDisposable CreateFile(string directoryPath, string fileName);
+        Task<string> ReadAllFile(string directoryPath, string fileName);
+        Task WriteToFile(IDisposable stream, string text);
+    }
+}
---
+++ Module3HW7/AsyncLogger/Services/FileService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using AsyncLogger.Services.Abstract;
+
+namespace AsyncLogger
+{
+    public class FileService : IFileService
+    {
+        private static readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
+        private static bool _start = true;
+        public IDisposable CreateFile(string dirPath, string fileName)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
+            var newFileName = $"{dirPath}/{fileName}";
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            if (_start && File.Exists(newFileName))
+            {
+                _start = false;
+                return new StreamWriter(newFileName, false);
+            }
+
+            return new StreamWriter(newFileName, true);
+        }
+
+        public async Task<string> ReadAllFile(string dirPath, string fileName)
+        {
+            var newFileName = $"{dirPath}/{fileName}";
+            var result = string.Empty;
+            using (var stream = new StreamReader(newFileName))
+            {
+                result = await stream.ReadToEndAsync();
+            }
+
+            await Task.Delay(1000);
+            return result;
+        }
+
+        public async Task WriteToFile(IDisposable stream, string text)
+        {
+            await _semaphoreSlim.WaitAsync();
+
+            await ((StreamWriter)stream).WriteLineAsync(text);
+            await ((StreamWriter)stream).FlushAsync();
+
+            _semaphoreSlim.Release();
+        }
+
+        public async Task CloseFile(IDisposable stream)
+        {
+            await Task.Run(() => ((StreamWriter)stream).Close());
+        }
+    }
+}
