5bc00b2e75a98376af24bfcbae9985b7e6dc0030
Andrew Kara <a.kara701@gmail.com>
2022-01-04 13:27:35 +0200

    Add logger Logic

---
+++ Module3HW7/AsyncLogger/Abstract/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using AsyncLogger.Helper;
+
+namespace AsyncLogger.Abstract
+{
+    public interface ILogger
+    {
+        event Action<string> StartBackup;
+        LoggerConfig LoggerConfig { get; set; }
+        IDisposable LoggerStream { get; set; }
+        Task LogInfo(LogType logType, string message);
+        Task Run();
+    }
+}
---
+++ Module3HW7/AsyncLogger/Helper/LogType.cs
@@ -0,0 +1,9 @@
+﻿namespace AsyncLogger.Helper
+{
+    public enum LogType
+    {
+        Info,
+        Warning,
+        Error
+    }
+}
