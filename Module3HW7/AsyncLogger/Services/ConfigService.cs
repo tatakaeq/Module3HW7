6018015caa472e3a3c9b302ac6a16d844e4bf6ff
Andrew Kara <a.kara701@gmail.com>
2022-01-04 13:26:44 +0200

    Add config files to project

---
+++ Module3HW7/AsyncLogger/Services/ConfigService.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using AsyncLogger.Helper;
+using AsyncLogger.Services.Abstract;
+using Newtonsoft.Json;
+
+namespace AsyncLogger
+{
+    public class ConfigService : IConfigService
+    {
+        public Config GetConfig()
+        {
+            var configFile = File.ReadAllText("../config.json");
+            var config = JsonConvert.DeserializeObject<Config>(configFile);
+            return config;
+        }
+    }
+}
